1ad116fc81405e642fd80c54bdc1460747b66888
mauribeh <mauribeh@unal.edu.co>
2023-02-24 12:08:07 -0500

    Update main.c
    Ejemplo de STM32

--- Semana3/Src/main.c
+++ Semana3/Src/main.c
@@ -18,12 +18,15 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+//Definicion de variables
+unsigned short estudiantes = 0;
+unsigned char counter_i = 0;
 
 int main(void)
 {
     /* Loop forever */
-	for(;;);
+	while(1){
+		estudiantes=estudiantes+2;
+		counter_i = -counter_i;
+	}
 }
