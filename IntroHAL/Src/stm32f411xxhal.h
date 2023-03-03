9fe5a37a0d591836992480d6e9e904d525b05148
mauribeh <mauribeh@unal.edu.co>
2023-03-03 11:46:13 -0500

    Configuracion de pines
    Programa de definicion de pines

---
+++ IntroHAL/Src/stm32f411xxhal.h
@@ -0,0 +1,14 @@
+/*
+ * stm32f411xxhal.h
+ *
+ *  Created on: Mar 3, 2023
+ *      Author: majo
+ */
+
+#ifndef STM32F411XXHAL_H_
+#define STM32F411XXHAL_H_
+
+
+
+#endif /* STM32F411XXHAL_H_ */
+//La informacion debe estar antes de esta linea de codigo
